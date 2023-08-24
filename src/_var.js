4bf6206fef8ba6df498e2552cbd86eb2686ea41d
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2023-08-24 07:53:27 +0500

    files deleted
    Extra files has deleted

--- src/_var.js
+++
@@ -1,5 +0,0 @@
-const Que = document.querySelector(".que");
-const opts = document.querySelector(".opts");
-const DesTxt = document.querySelector(".des_text span");
-const skipBtn = document.querySelector(".btns .skip_btn");
-const opt = document.querySelectorAll(".opts opt");
